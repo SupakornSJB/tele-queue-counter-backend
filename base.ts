bcdd0eed3fcf7fda728dab9c5c038295f76d7b1e
SupakornSJB <supakorn.sen@outlook.com>
2024-09-13 23:53:04 +0700

    Squashed 'src/dto/' changes from 15e901d..b3f18b9
    b3f18b9 feat: add sync all response to dto
    
    git-subtree-dir: src/dto
    git-subtree-split: b3f18b9a8cd78d1f695a7d0423e37fe110067fcf

--- base.ts
+++ base.ts
@@ -1,3 +1,16 @@
+import { PublicServerDTO } from "./server";
+import { TrafficDTOIncludeOwnership } from "./traffic";
+
 export class BaseDTO {
   userId: string;
+}
+
+export class SyncAllResponse {
+  servers: PublicServerDTO[];
+  traffics: TrafficDTOIncludeOwnership[];
+
+  constructor(servers: PublicServerDTO[], traffics: TrafficDTOIncludeOwnership[]) {
+    this.servers = servers;
+    this.traffics = traffics;
+  }
 }
